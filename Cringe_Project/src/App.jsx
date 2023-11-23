0c0c1e4eb55802b09cccda18480b69002b309ff5
Cricri Au <andcricri93@gmail.com>
2023-11-23 17:04:19 +0100

    css display page

--- Cringe_Project/src/App.jsx
+++ Cringe_Project/src/App.jsx
@@ -1,31 +1,33 @@
 import "./App.css";
-import { useState} from 'react';
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import Api from './Api';
-
+import Api from "./Api";
 
 function App() {
-  
-  const [joke , setJoke] = useState([])
-  const [test, setTest] = useState(true)
-  console.log(test)
+  const [joke, setJoke] = useState([]);
+  const [test, setTest] = useState(true);
+  console.log(test);
 
   return (
     <>
-  <Api joke={joke} setJoke={setJoke} test={test} />
-    <div className='presentation'>
-      <h1>Cringe Me</h1>
-      <p>Comment être malaisant dans toutes les situations</p>
-    </div>
-<div className='contenu'>
-  <button className="enFamille">EN FAMILLE </button>
-   <Link to ="display" ><button className="entreAmis">ENTRE AMIS </button></Link>
-   <button className="enAmoureux" onClick={()=> setTest(!joke)} >EN AMOUREUX</button>
+      <Api joke={joke} setJoke={setJoke} test={test} />
+      <div className="presentation">
+        <h1>Cringe Me</h1>
+        <p>Comment être malaisant dans toutes les situations</p>
+      </div>
+      <div className="contenu">
+        <button className="enFamille">EN FAMILLE </button>
+        <Link to="display">
+          <button className="entreAmis">ENTRE AMIS </button>
+        </Link>
+        <button className="enAmoureux" onClick={() => setTest(!joke)}>
+          EN AMOUREUX
+        </button>
         <button className="entreCollegues">ENTRE COLLEGUES </button>
         <button className="enPublic">EN PUBLIC</button>
         <button className="enPrive">EN PRIVE</button>
-   </div>
-   <div>{joke}</div>
+      </div>
+      <div>{joke}</div>
     </>
   );
 }
--- Cringe_Project/src/display.jsx
+++ Cringe_Project/src/display.jsx
@@ -1,17 +1,25 @@
-import Api from "./Api"
-import { useState } from "react"
+import Api from "./Api";
+import { useState } from "react";
+import "./Display.css";
 
-export default function Display(){
+export default function Display() {
+  const [joke, setJoke] = useState([]);
+  const [answer, setAnswer] = useState("");
+  console.log(joke);
 
-const [joke , setJoke] = useState([])
-const[answer, setAnswer] = useState("")
-console.log(joke)
-
-    return(
-        <div>
-        <Api joke={joke} setJoke={setJoke} setAnswer={setAnswer}/>
-        <div>{joke}</div>
-        <div>{answer}cc</div>
+  return (
+    <div className="mainBackground">
+      <section className="headerBackground">
+        <Api joke={joke} setJoke={setJoke} setAnswer={setAnswer} />
+        <div className="jokeBackground">
+          <div className="joke">{joke}</div>
+          <div className="answer">{answer}</div>
         </div>
-    )
+      </section>
+      <section className="buttonContainer">
+        <button>Retour</button>
+        <button>Suivant</button>
+      </section>
+    </div>
+  );
 }
