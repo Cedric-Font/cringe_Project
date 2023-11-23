aaa7ab2b1e4e6240fcdbb6995412879031268d5f
Cedric-Font <font.cedric38@gmail.com>
2023-11-23 20:23:09 +0100

    falling snow

--- Cringe_Project/src/App.jsx
+++ Cringe_Project/src/App.jsx
@@ -4,22 +4,34 @@ import Display from "./display";
 import Home from "./Home";
 
 
+
 function App() {
   
   const [categories, setCategories] = useState("Home")
+ 
 
   
  console.log(categories)
   return (
     <>
-   {categories === "Home" ? <Home setCategories={setCategories} />:null}
+    <div className="wrapper">
+    {categories === "Home" ? <Home setCategories={setCategories} />:null}
    {categories === "ENTRE AMIS" ? <Display categories={categories} />: null}
    {categories === "ENTRE COLLEGUES" ? <Display categories={categories} />: null}
    {categories === "EN PUBLIC" ? <Display categories={categories} />: null}
    {categories === "EN PRIVEE" ? <Display categories={categories} />: null}
    {categories === "EN FAMILLE" ? <Display categories={categories} />: null}
    {categories === "EN AMOUREUX" ? <Display categories={categories} />: null}
-    </>
+    
+     <div className="snow layer1 a"></div>
+     <div className="snow layer1"></div> 
+     <div className="snow layer2 a"></div>
+     <div className="snow layer2"></div>
+     <div className="snow layer3 a"></div>
+     <div className="snow layer3"></div>
+</div>
+</>
+
   );
 }
 
