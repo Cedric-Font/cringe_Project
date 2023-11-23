5604fc13f574c81a37414c06ae2ea0798a45cf3e
Cedric-Font <font.cedric38@gmail.com>
2023-11-23 14:28:18 +0100

    add link

--- Cringe_Project/src/Api.jsx
+++ Cringe_Project/src/Api.jsx
@@ -1,15 +1,19 @@
 import { useEffect } from "react";
 import BlaguesAPI from 'blagues-api';
 
-export default function Api({ joke, setJoke, setAnswer }){
+export default function Api({ joke, setJoke, setAnswer, categories1, categories2, categorie3 }){
    
   
     useEffect(()=>{
       async function funcBlague(){
         const blagues = new BlaguesAPI('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMzQzMTA2NTg0MjE1NDIwOTI4IiwibGltaXQiOjEwMCwia2V5IjoiYTU1U2NIYUNjMXBRUzZvRnJGT0pNSHhzSmVSbjJWWDczdG82MTNVcEZIRkU4V1htMzciLCJjcmVhdGVkX2F0IjoiMjAyMy0xMS0yM1QxMDoyNzo1NSswMDowMCIsImlhdCI6MTcwMDczNTI3NX0.E3o0oZialuzdEhuLpEfQPEqv1sA9Z82b8Je3C9-ugJo');
-        const blague = await blagues.randomCategorized(
-          blagues.categories.DARK
-        );
+        const blague = await blagues.random({
+  disallow: [
+    blagues.categories.DARK,
+    blagues.categories.LIMIT,
+    blagues.categories.BLONDES
+  ]
+});
         setJoke(blague.joke)
         setAnswer(blague.answer)
         console.log(blague.answer)
--- Cringe_Project/src/App.jsx
+++ Cringe_Project/src/App.jsx
@@ -9,23 +9,31 @@ function App() {
   
   const [joke , setJoke] = useState([])
   const [test, setTest] = useState(true)
+  const [categories1, setCategories1] = useState("")
+  const [categories2, setCategories2] = useState("")
+  const [categories3, setCategories3] = useState("")
+
+  function entreAmis(){
+    setCategories1("dev")
+    setCategories2("LIMIT")
+    setCategories3("BLONDES")
+  }
   console.log(test)
 
 
   return (
     <>
-  <Api joke={joke} setJoke={setJoke} test={test} />
+  <Api joke={joke} setJoke={setJoke} test={test} categories1={categories1} categories2={categories2} categories3={categories3} />
     <div className='presentation'>
       <h1>Cringe Me</h1>
       <p>Comment être malaisant dans toutes les situations</p>
     </div>
 <div className='contenu'>
-   <Link to ="display" ><button>ENTRE AMIS </button></Link>
-   
-   <button onClick={()=> setTest(!joke)} >EN AMOUREUX</button>
-   <button>ENTRE COLLEGUES </button>
-   <button>EN PUBLIC</button>
-   <button>EN PRIVEE</button>
+   <Link to ="display" ><button onClick={()=> entreAmis()} >ENTRE AMIS </button></Link>
+   <Link to ="display" ><button>EN AMOUREUX </button></Link>
+   <Link to ="display" ><button>ENTRE COLLEGUES</button></Link>
+   <Link to ="display" ><button>EN PUBLIC</button></Link>
+   <Link to ="display" ><button>EN PRIVEE</button></Link>
    </div>
 
    <div>{joke}</div>
