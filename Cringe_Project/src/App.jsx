2267768399e3e1430fc3cff3fa0feb9c5894bec4
Cedric-Font <font.cedric38@gmail.com>
2023-11-23 13:55:37 +0100

    display component

--- Cringe_Project/src/App.jsx
+++ Cringe_Project/src/App.jsx
@@ -1,42 +1,31 @@
 import './App.css'
-import { useState, useEffect } from 'react';
-import BlaguesAPI from 'blagues-api';
+import { useState} from 'react';
+import { Link } from "react-router-dom";
+import Api from './Api';
 
 
 function App() {
-  const [joke , setJoke] = useState([])
   
-useEffect(()=>{
-  async function funcBlague(){
-    const blagues = new BlaguesAPI('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMzQzMTA2NTg0MjE1NDIwOTI4IiwibGltaXQiOjEwMCwia2V5IjoiYTU1U2NIYUNjMXBRUzZvRnJGT0pNSHhzSmVSbjJWWDczdG82MTNVcEZIRkU4V1htMzciLCJjcmVhdGVkX2F0IjoiMjAyMy0xMS0yM1QxMDoyNzo1NSswMDowMCIsImlhdCI6MTcwMDczNTI3NX0.E3o0oZialuzdEhuLpEfQPEqv1sA9Z82b8Je3C9-ugJo');
-    const blague = await blagues.randomCategorized(
-      blagues.categories.DARK
-    );
-    console.log(blague)
-    setJoke(blague.joke)
-  }
-  funcBlague()
-},[])
-
-
+  const [joke , setJoke] = useState([])
+  const [test, setTest] = useState(true)
+  console.log(test)
 
   return (
     <>
-
+  <Api joke={joke} setJoke={setJoke} test={test} />
     <div className='presentation'>
       <h1>Cringe Me</h1>
       <p>Comment être malaisant dans toutes les situations</p>
     </div>
 <div className='contenu'>
-   <button>EN FAMILLE </button>
-   <button>ENTRE AMIS </button>
-   <button>EN AMOUREUX</button>
+   <Link to ="display" ><button>ENTRE AMIS </button></Link>
+   
+   <button onClick={()=> setTest(!joke)} >EN AMOUREUX</button>
    <button>ENTRE COLLEGUES </button>
    <button>EN PUBLIC</button>
    <button>EN PRIVEE</button>
    </div>
-
-    <div>{joke}</div> 
+   <div>{joke}</div>
 
     </>
   )
---
+++ Cringe_Project/src/display.jsx
@@ -0,0 +1,17 @@
+import Api from "./Api"
+import { useState } from "react"
+
+export default function Display(){
+
+const [joke , setJoke] = useState([])
+const[answer, setAnswer] = useState("")
+console.log(joke)
+
+    return(
+        <div>
+        <Api joke={joke} setJoke={setJoke} setAnswer={setAnswer}/>
+        <div>{joke}</div>
+        <div>{answer}</div>
+        </div>
+    )
+}
