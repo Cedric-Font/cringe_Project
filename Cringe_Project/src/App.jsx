21989e0c1d80d7336abcc81d3c494de3e2bc5034
celineblle <celineblle@outlook.fr>
2023-11-23 13:13:08 +0100

    creation des bouton, du titre et du texte de presentation de la page

--- Cringe_Project/src/App.jsx
+++ Cringe_Project/src/App.jsx
@@ -1,13 +1,28 @@
 import './App.css'
 
-function App() {
+
+
+ function App() {
 
 
   return (
     <>
-   
+    <div className='presentation'>
+      <h1>Cringe Me</h1>
+      <p>Comment être malaisant dans toutes les situations</p>
+    </div>
+<div className='contenu'>
+   <button>EN FAMILLE </button>
+   <button>ENTRE AMIS </button>
+   <button>EN AMOUREUX</button>
+   <button>ENTRE COLLEGUES </button>
+   <button>EN PUBLIC</button>
+   <button>EN PRIVEE</button>
+   </div>
     </>
   )
 }
 
 export default App
+
+
